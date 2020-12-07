7bafb1d0fef74b4b168fd71862cced6d11420439
Carlos Bautista de Jesus <cbautista@arssenasa.local>
2020-12-07 19:24:50 -0400

    Se agregan nuevos archivos y carpetas

---
+++ Models/DBConexion.php
@@ -0,0 +1,17 @@
+<?php 
+    // Usando PDO para la conexion la DB y acceso a los datos.
+
+    // Credenciales por defecto -> usuario BD
+    define('DB_HOST','localhost');
+    define('DB_USER','root');
+    define('DB_PASS','');
+    define('DB_NAME','test');
+
+    try {
+        
+        $PDOConnection = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+
+    } catch (PDOException $e) {
+        echo "Error en la conexión ". $e->getMessage();
+    }     
+?>
